f156aa720168fd08ac0539503160355ca99d2efc
Chae Nicholson <chae.nicholson@gmail.com>
2019-07-08 13:01:09 +0100

    Added functionality to aggregate the daily prices for all stocks (multiplied by number of shares held) for chart display of overall view

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,13 @@
     <p>Working on data structure setup</p>
     <p>Stock value: {{total_stock_value.toFixed(2)}}</p>
     <p>All shares summary: {{all_shares_summary}}</p>
+    <p>All shares aggregate historic: {{all_shares_aggr_hist}}</p>
+
+    <p>start date: {{historicalStockList[0]["historical"][0].date}}</p>
+    <p>first price: {{historicalStockList[0]["historical"][0].close}}</p>
+    <p>Number of days to loop through: {{historicalStockList[0]["historical"].length}}</p>
+    <p>Number of stocks to loop through: {{historicalStockList.length}}</p>
+    <p>ticker: {{historicalStockList[1].symbol}}</p>
   </div>
 </template>
 
@@ -12,7 +19,7 @@ export default {
     return {
       total_stock_value: 0,
       all_shares_summary: [],
-      all_shares_aggregate_historic: [],
+      all_shares_aggr_hist: [],
       // Hard coded seed data for unit testing - start
       stock_array: [
         {
@@ -27,27 +34,6 @@ export default {
         }
       ],
       historicalStockList: [
-        {
-          "symbol": "AAPL",
-          "historical": [
-              {
-              "date": "2014-06-13",
-              "close": 83.6603
-            },
-            {
-              "date": "2014-06-16",
-              "close": 84.5035
-            },
-            {
-              "date": "2019-07-04",
-              "close": 204.37
-            },
-            {
-              "date": "2019-07-05",
-              "close": 204.55
-            }
-          ]
-        },
         {
           "symbol": "GOOG",
           "historical": [
@@ -68,6 +54,28 @@ export default {
               "close": 1131.16
             }
           ]
+        },
+
+        {
+          "symbol": "AAPL",
+          "historical": [
+            {
+              "date": "2014-06-13",
+              "close": 83.6603
+            },
+            {
+              "date": "2014-06-16",
+              "close": 84.5035
+            },
+            {
+              "date": "2019-07-04",
+              "close": 204.37
+            },
+            {
+              "date": "2019-07-05",
+              "close": 204.55
+            }
+          ]
         }
       ]
       // Hard coded seed data for unit testing - end
@@ -76,10 +84,11 @@ export default {
   methods: {
     build_stock_summary_total_value() {
       // 1. Total value of shareholding
-      // for each share in stockarray
-        // determine index for relevant entry in historical data array
-        // locate last entry in historical data object entry for that stock and use the close price * share quantity to get total value of share.
-        // add them all together to get total value
+      // for each share in stock_array
+        // Determine index for relevant entry in historical data array
+        // Locate last entry in historical data object entry for that stock
+        // Use the close price * share quantity to get total value of shares
+        // add to running total to get total stock value
       // end
       // 2. Augment database info
       // Add current price and individual stock value onto info retrieved from database and make new array set available for the list function
@@ -109,16 +118,59 @@ export default {
         // Build summary list
         // Start building summary data to then push onto all_shares_summary
         temp_summ_data = this.stock_array[i];
-        temp_summ_data["curr_share_price"] = current_price.toFixed(2);
-        temp_summ_data["total_value"] = stock_value.toFixed(2);
+        temp_summ_data["price"] = current_price.toFixed(2);
+        temp_summ_data["totalvalue"] = stock_value.toFixed(2);
         this.all_shares_summary.push(temp_summ_data);
         // re-initialize for next round
         temp_summ_data = [];
+      }
+    },
+    build_aggregated_historical_object() {
+    // This function loops through all the historical data objects and aggregates the close * qty data for each date to allow a composite graph of all shares to be displayed.
+
+      let i;
+      let i2;
+      let curr_date;
+      let daily_total = 0;
+      let temp_aggr_data = null;
+      let ticker;
+      let qty;
+      let index;
+      let close_price;
+
+      // Use first historical element as driver for number of iterations (days of data) - all should be same as that's what's returned from api
+      let num_hist_days = this.historicalStockList[0]["historical"].length;
+      // number of stocks to loop through
+      let num_stocks = this.historicalStockList.length;
+
+      for (i = 0; i < num_hist_days; i++) {
+
+        for (i2 = 0; i2 < num_stocks; i2++) {
+          // Capture date
+          curr_date = this.historicalStockList[i2]["historical"][i].date;
+          // Capture ticker
+          ticker = this.historicalStockList[i2].symbol;
+          // Capture no of shares for this ticker
+          index =
+            this.stock_array.findIndex(
+              stock => stock.ticker === ticker)
+          qty = this.stock_array[index].quantity;
+          // Capture close current_price
+          close_price =  this.historicalStockList[i2]["historical"][i].close;
+          // Add into running total for this date
+          daily_total += (close_price * qty);
+        }
+        // Move daily total into temp daily aggr arra
+        temp_aggr_data = { "date": curr_date, "value": daily_total };
+        this.all_shares_aggr_hist.push(temp_aggr_data);
+        daily_total = 0;
+        temp_aggr_data = [];
       }
     }
   },
   mounted() {
     this.build_stock_summary_total_value();
+    this.build_aggregated_historical_object();
   }
 }
 </script>
