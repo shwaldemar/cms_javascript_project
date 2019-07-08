068f980f261fab6dc2034c9e919e5b59f425d63b
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-08 14:38:41 +0100

    form shows in browser but not hooked up to anything.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,14 @@
 <template lang="html">
-  <p>Hello from your valuation app Chae & Mike</p>
+  <div>
+    <p>Hello from your valuation app Chae & Mike</p>
+
+    <new-holding-form></new-holding-form>
+  </div>
 </template>
 
 <script>
 import StocksService from '@/services/StocksService';
+import NewHoldingForm from '@/components/NewHoldingForm';
 
 export default {
   name: 'app',
@@ -23,6 +28,9 @@ export default {
           .then(data => this.priceData.push(data))
       })
     })
+  },
+  components: {
+    'new-holding-form': NewHoldingForm
   }
 }
 </script>
---
+++ client/src/components/NewHoldingForm.vue
@@ -0,0 +1,61 @@
+<template lang="html">
+  <form>
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name" v-model="name">
+
+    <label for="quantity">Quantity:</label>
+    <input type="number" name="" value="" v-model="quantity">
+
+    <input type="submit" name="submit" value="Save">
+
+  </form>
+</template>
+
+<script>
+export default {
+  "name": "new-holding-form",
+  data: function(){
+    return {
+      name: "",
+      quantity: "",
+      newStockOptions: [
+        {
+          "symbol": "HPQ",
+          "name": "HP Inc."
+        },
+        {
+          "symbol": "F",
+          "name": "Ford Motor Company"
+        },
+        {
+          "symbol": "AMD",
+          "name": "Advanced Micro Devices Inc."
+        },
+        {
+          "symbol": "ORCL",
+          "name": "Oracle Corporation"
+        },
+        {
+          "symbol": "NKE",
+          "name": "Nike Inc."
+        },
+        {
+          "symbol": "PFE",
+          "name": "Pfizer Inc."
+        },
+        {
+          "symbol": "XOM",
+          "name": "Exxon Mobil Corporation",
+        },
+        {
+          "symbol": "CSCO",
+          "name": "Cisco Systems Inc.",
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
